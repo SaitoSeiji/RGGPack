078fa90990a6a8409e3a3eda9cb98525d388617f
DESKTOP-CMOH7M4\blueg <blueg0826@gmail.com>
2020-05-31 14:56:51 +0900

    made

--- RPGMake/Assets/Script/DataBase/DB/Static/CharcterDB.cs
+++ RPGMake/Assets/Script/DataBase/DB/Static/CharcterDB.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ChracterDB", menuName = "DataBases/DataBase/ChracterDB", order = 0)]
 public class CharcterDB : StaticDB
 {
-    [SerializeField] public List<CharcterDBData> _dataList;
+    [SerializeField] public List<CharcterDBData> _dataList=new List<CharcterDBData>();
     
     public override List<AbstractDBData> GetDataList(IEnable_initDB enable)
     {
