2e81150f357e21425b323866e83c72260edce1ba
DESKTOP-CMOH7M4\blueg <blueg0826@gmail.com>
2020-06-05 01:15:16 +0900

    レベルを実装した
    レベルを実装した
    レベルとパラメータを確認できる機能を付けたら完成

--- RPGMake/Assets/Script/DataBase/DBD/variable/PlayerDBData.cs
+++ RPGMake/Assets/Script/DataBase/DBD/variable/PlayerDBData.cs
@@ -2,47 +2,135 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using System;
+
+[System.Serializable]
+public class NeedExpList
+{
+    [SerializeField,NonEditable]int _maxlevel;
+    [SerializeField, NonEditable] int _firstExp;
+    [SerializeField, NonEditable] float _expRate;
+    [SerializeField, NonEditable] List<int> _needExpList;//i->i+1に必要な経験値
+
+    public NeedExpList(int maxLevel,int firstExp,float expRate)
+    {
+        _maxlevel = maxLevel;
+        _firstExp = firstExp;
+        _expRate = expRate;
+        _needExpList=SetNeedExpList();
+    }
+
+    public int this[int level]
+    {
+        get
+        {
+            if (level < _maxlevel) return _needExpList[level];
+            else return 0;
+        }
+    }
+
+    //checkLevelで必要な経験値
+    int GetTargetLevelExp_raw(int checkLevel)
+    {
+        return (int)(_firstExp * Mathf.Pow(_expRate, checkLevel - 1));
+    }
+    List<int> SetNeedExpList()
+    {
+        var result = new List<int>(_maxlevel + 1);
+        result.Add(0);
+        for (int i = 1; i < result.Capacity; i++)
+        {
+            result.Add(GetTargetLevelExp_raw(i));
+        }
+        return result;
+    }
+}
+
 [System.Serializable]
 public class SavedDBData_player : SavedDBData_char
 {
+    #region field
     [SerializeField,Space] public int _spMax;
     [SerializeField] public int _spNow;
     [SerializeField] public int _hpNow;
 
-    public SavedDBData_player()
+    [SerializeField] float _expRate;//経験値の増加量　とりあえず仮でa*ExpRate^levelの指数関数を採用
+    public float ExpRate
     {
+        get { return _expRate; }
+        set { _expRate = value / 1000f; }
+    }
+    [SerializeField] public int _firstExp;
 
+    [Serializable]
+    public class ParamGrowData
+    {
+        public int hp;
+        public int sp;
+        public int attack;
+        public int guard;
     }
+    [SerializeField]public ParamGrowData _paramGrowData=new ParamGrowData();
 
-    public SavedDBData_player(SavedDBData_player data) : base(data)
-    {
-        _spNow = data._spNow;
-        _spMax = data._spMax;
-        _hpNow = data._hpNow;
-    }
-    public SavedDBData_player(SavedDBData_char data) : base(data)
-    {
-    }
-
-    public new SavedDBData_player Clone()
-    {
-        return new SavedDBData_player(this);
-    }
-
-    public SavedDBData_player Copy(SavedDBData_char data)
-    {
-        var result =new SavedDBData_player(data);
-        result._spNow = _spNow;
-        result._spMax = _spMax;
-        result._hpNow = _hpNow;
-        return result;
-    }
+    [SerializeField] public int _level=1;
+    [SerializeField]public NeedExpList _needExpList;
+    #endregion
     //ここからデータへの変更をするもの
+    public void Init()
+    {
+        _needExpList = new NeedExpList(99, _firstExp, ExpRate);
+    }
+
     public void AddSkill(SkillDBData data)
     {
         if (_mySkillList.Contains(data)) return;
         _mySkillList.Add(data);
     }
+
+    //レベルの更新と上昇レベルの取得
+    public int UpdateLevel()
+    {
+        int up;
+        int templevel = _level;
+        for( up =0; true; up++)
+        {
+            var targetexp = GetTargetLevelExp_sum(templevel);
+            if (targetexp < _exp)templevel++;
+            else break;
+        }
+        _level += up;
+        UpdateParam(up);
+        return up;
+    }
+
+    void UpdateParam(int up)
+    {
+        _hpMax += up * _paramGrowData.hp;
+        _spMax += up * _paramGrowData.sp;
+        _attack += up * _paramGrowData.attack;
+        _guard += up * _paramGrowData.guard;
+        if (up > 0)
+        {
+            _hpNow = _hpMax;
+            _spNow = _spMax;
+        }
+    }
+    #region データへの参照をするもの
+    //checkLevelで必要な経験値
+    public int GetTargetLevelExp_raw(int checkLevel)
+    {
+        return _needExpList[checkLevel];
+    }
+    public int GetTargetLevelExp_sum(int checkLevel)
+    {
+        var result = 0;
+        for(int i = 1; i <= checkLevel; i++)
+        {
+            result += _needExpList[i];
+        }
+        return result;
+    }
+    #endregion
 }
 
 [CreateAssetMenu(fileName = "PlayerDBData", menuName = "DataBases/Data/PlayerDBData", order = 0)]
@@ -61,10 +149,13 @@ public class PlayerDBData : VariableDBData
     {
         var temp = (SavedDBData_char)_charData;
         Partial_CharcterDBData.UpdateMember(ref temp, ref _skillNameSet, data);
-        _charData = _charData.Copy(temp);
         _charData._spMax = data.GetData_int("spMax");
-        _charData._spNow = data.GetData_int("spNow");
-        _charData._hpNow = data.GetData_int("hpNow");
+        _charData._hpNow = _charData._hpMax;
+        _charData._spNow = _charData._spMax;
+        _charData.ExpRate = data.GetData_int("expRate");
+        _charData._firstExp = data.GetData_int("expFirst");
+        _charData._paramGrowData=UpdateMember_growData(data.GetData_list("paramGrow"));
+        _charData.Init();
     }
 
     public override void RateUpdateMemeber()
@@ -73,7 +164,52 @@ public class PlayerDBData : VariableDBData
 
         var temp = (SavedDBData_char)_charData;
         Partial_CharcterDBData.RateUpdateMemeber(ref temp, _skillNameSet);
-        _charData = _charData.Copy(temp);
         EditorUtility.SetDirty(this);
+    }
+    //growDataについてのupdateMember
+    //長くなったので分けた
+    static SavedDBData_player.ParamGrowData UpdateMember_growData(List<string> growlist)
+    {
+        //成長データ
+        var result = new SavedDBData_player.ParamGrowData();
+        foreach (var growdata in growlist)
+        {
+            try
+            {
+                var input = growdata.Split(' ');
+                int num = int.Parse(input[1]);
+                switch (input[0])
+                {
+                    case "hp":
+                        result.hp = num;
+                        break;
+                    case "sp":
+                        result.sp = num;
+                        break;
+                    case "attack":
+                        result.attack = num;
+                        break;
+                    case "guard":
+                        result.guard = num;
+                        break;
+                    default:
+                        Debug.LogWarning($"PlayerDB-paramGrow:想定されていないパラメーターがあります." +
+                            $"data={growdata}");
+                        break;
+                }
+            }
+            catch (Exception e) when (e is IndexOutOfRangeException ||
+                                     e is FormatException)
+            {
+                Debug.LogError($"PlayerDB-paramGrow:値に異常があります.:data={growdata}" +
+                    $"\n{e}");
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"PlayerDB-paramGrow:関連した異常があります.:data={growdata}" +
+                    $"\n{e}");
+            }
+        }
+        return result;
     }
 }
---
+++ RPGMake/Assets/Tests/Test_editormode/Editor/Test_PlayerDBData.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class Test_PlayerDBData
+    {
+        SavedDBData_player _myPlData;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _myPlData = new SavedDBData_player();
+        }
+
+        [TestCase(1, 10)]
+        [TestCase(2, 11)]
+        [TestCase(3, 12)]
+        public void Test_GetNextLevelExp_raw(int level, int exp)
+        {
+            _myPlData.ExpRate = 1100;
+            _myPlData._firstExp = 10;
+            _myPlData.Init();
+            var next = _myPlData.GetTargetLevelExp_raw(level);
+            Assert.AreEqual(exp, next);
+        }
+        [TestCase(1, 10)]
+        [TestCase(2, 21)]
+        [TestCase(3, 33)]
+        public void Test_GetNextLevelExp_sum(int level, int exp)
+        {
+            _myPlData.ExpRate = 1100;
+            _myPlData._firstExp = 10;
+            _myPlData.Init();
+            var next = _myPlData.GetTargetLevelExp_sum(level);
+            Assert.AreEqual(exp, next);
+        }
+        [TestCase(1,1,1)]
+        [TestCase(15,1,2)]
+        [TestCase(23,1,3)]
+        public void Test_UpdateLevel(int haveexp,int firstlevel,int resultlevel)
+        {
+            _myPlData.ExpRate = 1100;
+            _myPlData._firstExp = 10;
+            _myPlData._exp = haveexp;
+            _myPlData._level = firstlevel;
+            _myPlData.Init();
+            var up = _myPlData.UpdateLevel();
+            Assert.AreEqual(resultlevel, _myPlData._level);
+            Assert.AreEqual(resultlevel-firstlevel, up);
+        }
+    }
+}
